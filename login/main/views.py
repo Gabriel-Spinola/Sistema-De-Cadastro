89290da144bb2b52132dda65ee141a61734b307a
gseven-gdv <zerogravitystd@gmail.com>
2022-03-01 15:09:04 -0300

    admin dashboard

--- login/main/views.py
+++ login/main/views.py
@@ -8,3 +8,6 @@ def index(response, id):
     item = ls.item_set.get(id=id)
 
     return HttpResponse('<h1>%s</h1><h2>%s</h2>' % (ls.name, str(item.text)))
+
+# Each item added to the database have an increasing id, starting from 1
+# i.e. if we have 1 item, the id will be 1, 2 items id 2...
