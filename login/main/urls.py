33e9ae7ebe060943bf8aca7501dcd01405227dbb
gseven-gdv <zerogravitystd@gmail.com>
2022-03-01 13:15:27 -0300

    Hello, World!

--- login/main/urls.py
+++ login/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views # Import views from the current directory
 
 # Define the views path 
-url_pattern = [
+urlpatterns = [
     path('', views.index, name='index') # If home go to views.index page
 ]
 
