baf3fa189065d98fb4e34db0e8cec4da34c78de4
gseven-gdv <zerogravitystd@gmail.com>
2022-03-02 16:10:23 -0300

    -m

--- login/register/forms.py
+++ login/register/forms.py
@@ -3,6 +3,9 @@ from django import forms
 class CreateNewUser (forms.Form):
     username = forms.CharField(label="Username", max_length=100)
     password = forms.CharField(label="Password", max_length=100)
+    #is_checked = forms.CheckboxInput(="Remember me")
 
-class Login (CreateNewUser):
-    pass
+
+class Login (forms.Form):
+    username = forms.CharField(label="Username", max_length=100)
+    password = forms.CharField(label="Password", max_length=100)
--- login/register/views.py
+++ login/register/views.py
@@ -7,23 +7,38 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def sign_up(response):
+    show_error = False
+    error_log = ""
+
     if response.method == 'POST':
         form = CreateNewUser(response.POST)
 
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
+            # is_checked = form.cleaned_data['is_checked']
 
-            users_list = User(username=username, password=password)
-            users_list.save()
-        
-        return HttpResponseRedirect('/register/login')
+            if User.objects.filter(username=username).count() >= 1:
+                print(User.objects.filter(username=username, password=password).count())
+                show_error = True
+                error_log = "Usuario já existente"
+            else:
+                show_error = False
+                users_list = User(username=username, password=password)
+                users_list.save()
+
+                return HttpResponseRedirect('/register/login')
     else:
         form = CreateNewUser()
 
     return render(response, 'register/signup.html', {
-        'form': form
+        'form': form,
+        'show_error': show_error,
+        'error_log': error_log,
     })
+
+def password_check():
+    pass
 
 def login(response):
     if response.method == 'POST':
@@ -33,13 +48,11 @@ def login(response):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
-            #users_list = User()
-
             try: 
                 User.objects.get(username=username, password=password)
 
                 return HttpResponseRedirect('/')
-            except:
+            except ValueError:
                 return HttpResponse('<h2>ERROR</h2>')
     else:
         form = Login()
@@ -47,3 +60,6 @@ def login(response):
     return render(response, 'register/login.html', {
         'form': form
     })
+
+def log_out(response):
+    pass
